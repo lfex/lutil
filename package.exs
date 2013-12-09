dd4b9dbe642a977628b209809e641caf89c5129a
Duncan McGreggor <oubiwann@gmail.com>
2013-12-08 20:57:30 -0600

    Fixed list.

--- package.exs
+++ package.exs
@@ -1,7 +1,7 @@
 Expm.Package.new(name: "lfe-utils",
                  description: "Utility functions for LFE",
                  version: "0.1.2",
-                 keywords: ["LFE" "Lisp" "Utilities"],
+                 keywords: ["LFE", "Lisp", "Utilities"],
                  maintainers: [[name: "Duncan McGreggor",
                                 email: "oubiwann@cogitat.io"]],
                  repositories: [[github: "lfe/lfe-utils"]])
