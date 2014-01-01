c3ccc783ec2da22d677566bbd5d44b62da5d3d0b
Duncan McGreggor <oubiwann@gmail.com>
2014-01-01 14:53:18 -0600

    Bumped version number.

--- package.exs
+++ package.exs
@@ -1,6 +1,6 @@
 Expm.Package.new(name: "lfe-utils",
                  description: "Utility functions for LFE",
-                 version: "0.1.2",
+                 version: "0.1.3",
                  keywords: ["LFE", "Lisp", "Utilities"],
                  maintainers: [[name: "Duncan McGreggor",
                                 email: "oubiwann@cogitat.io"]],
