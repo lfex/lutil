7007116c813df4077e9c3b304ec1707add28dee7
Duncan McGreggor <duncan@cogitat.io>
2014-06-17 09:09:57 -0700

    Updated expm tags.

--- package.exs
+++ package.exs
@@ -1,7 +1,7 @@
 Expm.Package.new(name: "lfe-utils",
                  description: "Utility functions for LFE",
                  version: "0.1.7",
-                 keywords: ["LFE", "Lisp", "Utilities"],
+                 keywords: ["LFE", "Lisp", "Utility", "Library"],
                  maintainers: [[name: "Duncan McGreggor",
                                 email: "oubiwann@cogitat.io"]],
                  repositories: [[github: "lfe/lfe-utils"]])
