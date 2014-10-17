611d29c95c801319c5901e658c4bbc83a85e3580
Døkkarr Hirðisson <dokkarr@lfe.io>
2014-10-17 12:25:54 -0500

    Bumped to version 0.3.0.

--- package.exs
+++ package.exs
@@ -1,6 +1,6 @@
 Expm.Package.new(name: "lutil",
-                 description: "Utility functions for LFE",
-                 version: "0.2.9",
+                 description: "Utility functions and macros for LFE",
+                 version: "0.3.0",
                  keywords: ["LFE", "Lisp", "Utility", "Library"],
                  maintainers: [[name: "Duncan McGreggor",
                                 email: "oubiwann@cogitat.io"]],
