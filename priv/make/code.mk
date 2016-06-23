0980eb2c846f8c473c1bee47bb7b5d019c4f1607
Eric Bailey <eric@ericb.me>
2016-06-23 02:56:28 -0500

    Ignore _* and update code.mk
    KISS and symlink self to test profile lib dir.

--- priv/make/code.mk
+++ priv/make/code.mk
@@ -1,8 +1,18 @@
-compile:
-	rebar3 compile
+lib    := $(notdir $(PWD))
+_build := $(PWD)/_build
 
-check:
-	@rebar3 as test eunit
+compile:
+	@rebar3 compile
+
+check: compile
+	@mkdir -p _build/test/lib
+	@ln -sf $(_build)/default/lib/$(lib) $(_build)/test/lib
+	@rebar3 eunit
+
+# check: compile
+# 	-@rebar3 as default+test compile
+# 	@./priv/scripts/setup_test_env.sh
+# 	@rebar3 as default+test eunit
 
 repl:
 	@rebar3 as dev compile
